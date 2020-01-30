6338854aee399cb7abda61bf4b2d265b2eff3575
Slav Basharov <slavbacharov@gmail.com>
2020-01-30 04:49:20 -0800

    Added custom icon animation

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * Keep track of the StumbleRevived tab
+ * Keep track of the StumbleUponAwesome tab
  */
 var stumbleTabId = null;
 
@@ -50,11 +50,148 @@ function loadUrl() {
   rawFile.send(null);
 
 }
+var interval = 0;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function animateIcon() {
+  const size = 48;
+  var canvas = document.createElement('canvas')
+  canvas.setAttribute('id', 'canvas')
+  canvas.width = size
+  canvas.height = size
+  canvas.style.position = 'absolute'
+  canvas.style.top = '0'
+  canvas.style.left = '0'
+
+  var context = canvas.getContext('2d');
+  var lines = 8;
+
+  var boxWidth = size;
+  var boxHeight = size;
+
+  var iconWidth = boxWidth * 0.7;
+  var iconHeight = boxHeight * 0.9;
+  var iconHalfHeight = iconHeight / 2;
+  var iconHalfWidth = iconWidth / 2;
+
+  context.beginPath();
+  context.fillStyle = "red";
+  context.strokeRect(0, 0, boxWidth, boxHeight);
+  context.strokeStyle = "green"
+  context.strokeRect((boxWidth - iconWidth) / 2, (boxHeight - iconHeight) / 2, iconWidth, iconHeight);
+  context.translate(((boxWidth - iconWidth) / 2), (boxHeight - iconHeight) / 2)
+
+  for (var i = 0; i < lines; i++) {
+    await sleep(20);
+    context.clearRect(0, 0, boxWidth, boxHeight);
+    // Within icon bounds now
+    switch (i) {
+      case 0:
+        context.beginPath();
+        context.moveTo(iconWidth, 0);
+        break;
+      case 1:
+        context.beginPath();
+        context.moveTo(iconWidth, 0);
+        context.moveTo(iconWidth, 0);
+        break;
+      case 2:
+        context.beginPath();
+        context.moveTo(iconWidth, 0);
+        context.moveTo(iconWidth, 0);
+        context.lineTo(0, iconHalfHeight);
+        break;
+      case 3:
+        context.beginPath();
+        context.moveTo(iconWidth, 0);
+        context.moveTo(iconWidth, 0);
+        context.lineTo(0, iconHalfHeight);
+        context.lineTo(iconHalfWidth, iconHalfHeight);
+        context.strokeStyle = '#27d7dd';
+        break;
+      case 4:
+        context.beginPath();
+        context.moveTo(iconWidth, 0);
+        context.moveTo(iconWidth, 0);
+        context.lineTo(0, iconHalfHeight);
+        context.lineTo(iconHalfWidth, iconHalfHeight);
+        context.lineTo(0, iconHeight);
+        context.strokeStyle = "#8b27dd";
+        break;
+      case 5:
+        context.beginPath();
+        context.moveTo(iconWidth, 0);
+        context.moveTo(iconWidth, 0);
+        context.lineTo(0, iconHalfHeight);
+        context.lineTo(iconHalfWidth, iconHalfHeight);
+        context.lineTo(0, iconHeight);
+        context.lineTo(iconWidth, iconHalfHeight);
+        context.strokeStyle = "#e6f7de";
+        break;
+      case 6:
+        context.beginPath();
+        context.moveTo(iconWidth, 0);
+        context.moveTo(iconWidth, 0);
+        context.lineTo(0, iconHalfHeight);
+        context.lineTo(iconHalfWidth, iconHalfHeight);
+        context.lineTo(0, iconHeight);
+        context.lineTo(iconWidth, iconHalfHeight);
+        context.lineTo(iconHalfWidth, iconHalfHeight);
+        context.strokeStyle = "#e6f7de";
+        break;
+      case 7:
+        context.beginPath();
+        context.moveTo(iconWidth, 0);
+        context.moveTo(iconWidth, 0);
+        context.lineTo(0, iconHalfHeight);
+        context.lineTo(iconHalfWidth, iconHalfHeight);
+        context.lineTo(0, iconHeight);
+        context.lineTo(iconWidth, iconHalfHeight);
+        context.lineTo(iconHalfWidth, iconHalfHeight);
+        context.closePath()
+        context.strokeStyle = "#e07128";
+        break;
+    }
+    context.lineWidth = 2.5;
+    // context.strokeStyle = 'rgba(216, 151, 131, 1)';
+    context.stroke();
+
+    var imageData = context.getImageData((boxWidth - iconWidth) / 2, (boxHeight - iconHeight) / 2, iconWidth, iconHeight);
+
+    chrome.browserAction.setIcon({
+      imageData: imageData
+    });
+
+  }
+
+  await sleep(200);
+  context.fillStyle = 'rgba(216, 151, 131, 1)';
+  context.fill();
+
+  var imageData = context.getImageData((boxWidth - iconWidth) / 2, (boxHeight - iconHeight) / 2, iconWidth, iconHeight);
+  chrome.browserAction.setIcon({
+    imageData: imageData
+  });
+
+  await sleep(400);
+  chrome.browserAction.setIcon({
+    imageData: null, 
+    path: "./images/icon_16.png"
+  });
+}
+
+function resetIcon() {
+  
+}
 
 // Load a page on click
 chrome.browserAction.onClicked.addListener(
   function (tab) {
     loadUrl()
+    animateIcon();
   }
 );
 
