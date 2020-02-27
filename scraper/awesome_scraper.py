1dcce5b37b5f7e639c12bb3235bf1e35842a85e2
Slav Basharov <slavbacharov@gmail.com>
2020-02-27 02:22:23 -0800

    Updated scraper to get link text, and .pdf links now included
    Now up to 32,146 URLs

--- scraper/awesome_scraper.py
+++ scraper/awesome_scraper.py
@@ -11,16 +11,38 @@ awesome_readmes = []
 all_urls = {}
 invalid_urls_count = 0
 
+IGNORED_EXTENSIONS = [
+    # archives
+    '7z', '7zip', 'bz2', 'rar', 'tar', 'tar.gz', 'xz', 'zip',
+
+    # images
+    'mng', 'pct', 'bmp', 'gif', 'jpg', 'jpeg', 'png', 'pst', 'psp', 'tif',
+    'tiff', 'ai', 'drw', 'dxf', 'eps', 'ps', 'svg', 'cdr', 'ico',
+
+    # audio
+    'mp3', 'wma', 'ogg', 'wav', 'ra', 'aac', 'mid', 'au', 'aiff',
+
+    # video
+    '3gp', 'asf', 'asx', 'avi', 'mov', 'mp4', 'mpg', 'qt', 'rm', 'swf', 'wmv',
+    'm4a', 'm4v', 'flv', 'webm',
+
+    # office suites
+    'xls', 'xlsx', 'ppt', 'pptx', 'pps', 'doc', 'docx', 'odt', 'ods', 'odg',
+    'odp',
+
+    # other
+    'css', 'exe', 'bin', 'rss', 'dmg', 'iso', 'apk'
+]
 class BlogSpider(scrapy.Spider):
     name = 'blogspider'
     start_urls = ['https://github.com/sindresorhus/awesome']
     print("Scraping...")
 
     def parse(self, response):
-        if os.path.exists("categories.txt"):
-            os.remove("categories.txt")
-        if os.path.exists("urls.txt"):
-            os.remove("urls.txt")
+        if os.path.exists("list_of_lists.txt"):
+            os.remove("list_of_lists.txt")
+        if os.path.exists("urls_with_descriptions.txt"):
+            os.remove("urls_with_descriptions.txt")
 
         global pages_count, urls_count
 
@@ -33,18 +55,18 @@ class BlogSpider(scrapy.Spider):
             unique=True
         )
 
-        with open("categories.txt", "a") as urls_file:
+        with open("list_of_lists.txt", "a") as urls_file:
             links = extractor.extract_links(response)
             for link in links:
                 if (re.match(r'(.*readme$)', link.url)):
                     urls_file.write("\n" + str(link.url))
                     awesome_readmes.append(link.url)
                     pages_count += 1
-                    yield scrapy.Request(link.url, callback=self.parse_readme_contents)
+                    yield scrapy.Request(link.url, callback=self.parse_readme_contents, cb_kwargs=dict(list_url=link.url, list_name=link.text))
 
             print("Done. Number of links:" + str(len(links)))
 
-    def parse_readme_contents(self, response):
+    def parse_readme_contents(self, response, list_url, list_name):
         global pages_count, urls_count
 
         exclude_sites = [
@@ -65,36 +87,26 @@ class BlogSpider(scrapy.Spider):
                 "coinbin.org",
                 "patreon.com"
             ],
+            deny_extensions=IGNORED_EXTENSIONS,
             restrict_xpaths=["//article"],
             restrict_css=[],
             unique=True
         )
 
-        with open("urls.txt", "a") as urls_file:
+        with open("urls_with_descriptions.txt", "a") as urls_file:
             # urls_file.write("\nPAGE:" + response.css('title::text').get() + "|" + response.url)
+            print("\rTotal urls: {}".format(len(all_urls)))
 
-            links = extractor.extract_links(response)
+            links=extractor.extract_links(response)
             for link in links:
                 # Avoid internal hashlinks
                 if (filter(lambda site_name: site_name in link.url, exclude_sites) and '#' not in link.url):
-                    url = link.url
-                    try:
-                        urllib.request.urlopen(url)
-                    except urllib.error.HTTPError as e:
-                        # Return code error (e.g. 404, 501, ...)
-                        print('HTTPError: {}'.format(e.code))
-                        invalid_urls_count += 1
-                    except urllib.error.URLError as e:
-                        # Not an HTTP-specific error (e.g. connection refused)
-                        print('URLError: {}'.format(e.reason))
-                        invalid_urls_count += 1
-                    else:
-                        # Add if not duplicate
-                        if (link.url not in all_urls):
-                                all_urls[link.url] = 1
-                                urls_file.write("\n" + str(url))
-                                urls_count += 1
-
+                    url=link.url
+                    if (link.url not in all_urls):
+                        all_urls[link.url]=1
+                        urls_file.write("\n{},{},{},{}".format(str(url), str(link.text).strip(), str(list_url), str(list_name).strip()))
+                        urls_count += 1
+                        
     def closed(self, reason):
         self.logger.debug(
             "Done processing awesome pages\nParsed %s urls from %s pages", urls_count, pages_count)
--- scraper/utils.py
+++ scraper/utils.py
@@ -12,7 +12,13 @@ other_errors = []
 chunks = []
 
 def clean_chunk(idx, chunk):
-    for url in chunk:
+    for line in chunk:
+        properties = line.split(',')
+        url = properties[0]
+        url_name = properties[1]
+        list_url = properties[2]
+        list_name = properties[3]
+
         try:
             all_urls.append(url)
             conn = urllib.request.urlopen(url, timeout=5)
@@ -21,20 +27,20 @@ def clean_chunk(idx, chunk):
         except urllib.error.URLError as e:
             other_errors.append(url)
         else:
-            valid_urls.append(url)
+            valid_urls.append(line)
         print("\rTotal urls: {}  Valid urls: {} Invalid urls: {}  HTTP errors: {}  Other errors: {}" \
               .format(len(all_urls), len(valid_urls), len(all_urls) - len(valid_urls), len(http_errors), len(other_errors)), end="")
 
 def merge_chunks():
-    with open ('../extension/urls_cleaned.txt', 'a') as f:
+    with open ('urls_with_descriptions_cleaned.txt', 'a') as f:
         for url in valid_urls:
             f.write(url)
     
 if __name__ == "__main__":
-    with open("../extension/urls.txt", 'r') as f:
+    with open("urls_with_descriptions.txt", 'r') as f:
 
-        if os.path.exists("../extension/urls_cleaned.txt"):
-            os.remove("../extension/urls_cleaned.txt")
+        if os.path.exists("urls_with_descriptions_cleaned.txt"):
+            os.remove("urls_with_descriptions_cleaned.txt")
 
         urls = f.readlines()
         chunks = [urls[x:x+100] for x in range(0, len(urls), 100)]
